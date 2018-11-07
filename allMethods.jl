cbed54b56d9771625901dd4c8890cfa17637ebf2
Emre Karaman <emrkaraman@gmail.com>
2018-11-07 11:58:06 +0100

    Update allMethods.jl

--- allMethods.jl
+++ allMethods.jl
@@ -686,7 +686,7 @@ function runSTBayesPR(phenoDataInRef::DataFrame,phenoDataInVal::DataFrame,genoDa
     return r_Bayes, varUhat, varR, varSNP
 end
 
-function checkBias(Y,X,tempData)
+function checkBias(tempData)
     ols = lm(@formula(Y ~ X),tempData)
     print(ols)
     tstat = (coef(ols)[2]-1.0)/(stderror(ols)[2])
